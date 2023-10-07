7b6ff480677b6b3406e819938b2c40c1c9818ee1
Rahmadika S Setiawan <dikakewren@outlook.com>
2023-10-07 13:47:06 +0700

    Initial commit

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Algorithm Soft</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="{{ asset('assets/images/icons/favicon.ico') }}"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/animate/animate.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/select2/select2.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/util.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/main.css') }}">
+<!--===============================================================================================-->
+</head>
+<body>
+
+
+	<div class="bg-img1 size1 overlay1" style="background-image: url('https://onward.justia.com/wp-content/uploads/2022/03/Law-Website-Under-Maintenance-1024x538.png');">
+		<div class="size1 p-l-15 p-r-15 p-t-30 p-b-50">
+			<div class="flex-w flex-sb-m p-l-75 p-r-60 p-b-165 respon1">
+
+			</div>
+
+			<div class="wsize1 m-lr-auto">
+				<h1 class="txt-center l1-txt1 p-b-60">
+					Kami sedang melakukan pemeliharaan rutin pada website kami untuk meningkatkan kualitas layanan. Mohon maaf atas ketidaknyamanannya. Kami akan segera kembali dalam waktu Terima kasih atas pengertian Anda.</span>
+				</h1>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
